436fc353caa1f1314bc66ffffed4bc65693f5ca4
Siddiquiraheem527 <Siddiquiraheem527@gmail.com>
2025-12-22 16:41:57 +0530

    fix: Update AuthGuard to allow access to the home route

--- src/app/components/AuthGuard.jsx
+++ src/app/components/AuthGuard.jsx
@@ -11,7 +11,7 @@ export default function AuthGuard({ children }) {
     const token = localStorage.getItem("token");
 
     // Public routes
-    if (pathname === "/login" || pathname === "/register") {
+    if (pathname === "/login" || pathname === "/register"|| pathname === "/") {
       return;
     }
 
